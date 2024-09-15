eebeba76dfc7038d1dc2a317662e498eb3246ab8
samunstein <samu.p.jussila@gmail.com>
2024-09-15 10:20:51 +0300

    Rename test client

--- clientmain.py
+++ clientmain.py
@@ -1,10 +1,10 @@
 from config import HOST, PORT
 from connection.common import OpenSocket
 from game.gameclient import PlayerClient
-from game.logic.clients import ConsoleClient
+from game.logic.clients import ExtremelySimpleTestClient
 
 if __name__ == "__main__":
     connection = OpenSocket.new(HOST, PORT)
-    logic = ConsoleClient()
+    logic = ExtremelySimpleTestClient()
     client = PlayerClient(connection, logic)
     client.run()
